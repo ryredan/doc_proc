39146efa2672105a30783787813bf774ea6459c4
Paulo Sergio Nogueira <ryredan@gmail.com>
2020-10-21 15:00:32 -0300

    Initial Commit

--- src/main/java/com/paulosrg/docproc/DocumentIdentificator.java
+++ src/main/java/com/paulosrg/docproc/DocumentIdentificator.java
@@ -5,14 +5,27 @@
  */
 package com.paulosrg.docproc;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 
@@ -22,48 +35,55 @@ import org.apache.pdfbox.text.PDFTextStripper;
  */
 public class DocumentIdentificator {
     
-    private HashMap<String, Set> dictionaries = new HashMap<>();
-
-    public DocumentIdentificator() {
-
-        dictionaries.put("Cartão de Ponto", new HashSet<>(Arrays.asList(new String[]{
-            "Cartão", "Ponto",
-            "Empregador", "Atividade", "Endereço.", "Empregado",
-            "Cargo", "Setor", "Periodo", "DATA",
-            "SEM", "ENTRADA", "SAIDA", "OBSERVAÇÕES",
-            "SEG", "TER", "QUA", "QUI",
-            "SEX", "SAB", "DOM"
-        })));
-        
-        dictionaries.put("Termo de Responsibilidade", new HashSet<>(Arrays.asList(new String[]{
-            "TERMO", "RESPONSABILIDADE", "RECEBIMENTO", "CRACHÁ",
-            "portador", "recebi", "identificação", "conhecimento",
-            "seguintes", "normas", "utilização", "funcional"
-        })));
-        
-        dictionaries.put("Folha de Pagamento", new HashSet<>(Arrays.asList(new String[]{
-            "Folha", "Mensal", "Código", "Nome",
-            "Funcionário", "Descrição", "Referência", "Vencimentos",
-            "Descontos", "Salário", "Base", "F.G.T.S",
-            "Faixa", "IRRF"
-        })));
+    private HashMap<String, Set> dictionaries;
+    
+    public DocumentIdentificator(){
+        dictionaries = new HashMap<>();
     }
     
-    public String checkDocumentType(File file) throws IOException{
+    public DocumentIdentificator(File dictionary) {
+        try {
+            this.loadDictionary(dictionary);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(DocumentIdentificator.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public Object[] checkDocumentType(File file) throws IOException{
         PDDocument doc = PDDocument.load(file);
         PDFTextStripper textStripper = new PDFTextStripper();
         String[] extractedText = textStripper.getText(doc).split("[^\\p{L}\\p{Nd}]+");
         Set<String> docSet = new HashSet<>(Arrays.asList(extractedText));
-        HashMap<String, Float> results = new HashMap<>();
+        //HashMap<String, Float> results = new HashMap<>();
+        String tipoDeDocumento = "";
+        float porcentagem = 0;
         for(Iterator<Map.Entry<String, Set>> it = dictionaries.entrySet().iterator(); it.hasNext(); ){
             Map.Entry<String, Set> current = it.next();
             Set<String> currentType = (Set<String>)((HashSet)current.getValue()).clone();
             currentType.retainAll(docSet);
-            results.put(current.getKey(), (currentType.size()/(float)current.getValue().size())*100);
+            float pc = (currentType.size()/(float)current.getValue().size())*100;
+            if(pc > porcentagem){
+                porcentagem = pc;
+                tipoDeDocumento = current.getKey();
+            }
         }
+
         doc.close();
-        return results.toString();
-        
+        //return results.toString();
+        return new Object[]{tipoDeDocumento, porcentagem};
+    }
+    
+    public void loadDictionary(File jsonfile) throws FileNotFoundException{
+        JsonObject jsobj = new Gson().fromJson(new InputStreamReader(new FileInputStream(jsonfile), Charset.forName("UTF-8")), JsonObject.class);
+        dictionaries = new HashMap<>();
+        for (Entry<String, JsonElement> nextElement : jsobj.entrySet()) {
+            String docName = nextElement.getKey();
+            ArrayList<String> values = new ArrayList<>();
+            for (JsonElement nextValue : nextElement.getValue().getAsJsonArray()) {
+                values.add(nextValue.getAsString());
+            }
+            dictionaries.put(docName, new HashSet<>(values));
+        }
     }
     
 }
--- src/main/java/com/paulosrg/docproc/MainWindow.java
+++ src/main/java/com/paulosrg/docproc/MainWindow.java
@@ -5,7 +5,9 @@
  */
 package com.paulosrg.docproc;
 
-import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.File;
+import java.io.FilenameFilter;
+import javax.swing.DefaultListModel;
 
 /**
  *
@@ -21,14 +23,20 @@ public class MainWindow extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jFileChooser1 = new javax.swing.JFileChooser();
+        pdfFolderChooser = new javax.swing.JFileChooser();
+        dictionaryChooser = new javax.swing.JFileChooser();
         jPanel1 = new javax.swing.JPanel();
         jTextField1 = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jList1 = new javax.swing.JList<>();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
 
-        jFileChooser1.setFileFilter(new FileNameExtensionFilter("Portable Document File", "pdf"));
+        pdfFolderChooser.setFileSelectionMode(javax.swing.JFileChooser.DIRECTORIES_ONLY);
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -41,6 +49,13 @@ public class MainWindow extends javax.swing.JFrame {
             }
         });
 
+        jButton2.setText("Iniciar");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -50,6 +65,8 @@ public class MainWindow extends javax.swing.JFrame {
                 .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 245, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton2)
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
@@ -58,14 +75,16 @@ public class MainWindow extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton1))
+                    .addComponent(jButton1)
+                    .addComponent(jButton2))
                 .addContainerGap())
         );
 
         jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder("Análise"));
 
-        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("Selecione um arquivo");
+        jList1.setModel(new DefaultListModel<String>()
+        );
+        jScrollPane1.setViewportView(jList1);
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
@@ -73,16 +92,30 @@ public class MainWindow extends javax.swing.JFrame {
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jScrollPane1)
                 .addContainerGap())
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 161, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 179, Short.MAX_VALUE)
                 .addContainerGap())
         );
+
+        jMenu1.setText("Arquivo");
+
+        jMenuItem1.setText("Carregar dicionário");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem1);
+
+        jMenuBar1.add(jMenu1);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -91,17 +124,17 @@ public class MainWindow extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
+                .addContainerGap(17, Short.MAX_VALUE)
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
 
@@ -109,12 +142,31 @@ public class MainWindow extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        int result = jFileChooser1.showOpenDialog(this);
-        if(result == jFileChooser1.APPROVE_OPTION){
-            jTextField1.setText(jFileChooser1.getSelectedFile().getAbsolutePath());
-            jLabel1.setText(new DocumentIdentificator().checkDocumentType(jFileChooser1.getSelectedFile()));
+        int result = pdfFolderChooser.showOpenDialog(this);
+        if(result == pdfFolderChooser.APPROVE_OPTION){
+            jTextField1.setText(pdfFolderChooser.getSelectedFile().getAbsolutePath());
         }
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        File[] files = new File(jTextField1.getText()).listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.toLowerCase().endsWith(".pdf");
+            }
+        });
+        for (File file : files) {
+            Object[] arquivoAtual = this.docIde.checkDocumentType(file);
+            ((DefaultListModel<String>)jList1.getModel()).addElement(String.format("%s: %s (%.2f%%)", file.getName(), arquivoAtual[0], arquivoAtual[1]));
+        }
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+        int result = dictionaryChooser.showOpenDialog(this);
+        if(result == dictionaryChooser.APPROVE_OPTION){
+            this.docIde.loadDictionary(dictionaryChooser.getSelectedFile());
+        }
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -150,13 +202,19 @@ public class MainWindow extends javax.swing.JFrame {
             }
         });
     }
-
+    private DocumentIdentificator docIde = new DocumentIdentificator();
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JFileChooser dictionaryChooser;
     private javax.swing.JButton jButton1;
-    private javax.swing.JFileChooser jFileChooser1;
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JList<String> jList1;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextField jTextField1;
+    private javax.swing.JFileChooser pdfFolderChooser;
     // End of variables declaration//GEN-END:variables
 }
